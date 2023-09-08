e8d67846416e1087166465ed1510b96dfb4f5684
Dmitri Timosin <heliophob@gmail.com>
2023-09-08 13:51:53 +0300

    Homework number 2 code written

---
+++ src/main/java/homework_nr_2/HomeworkNr2.java
@@ -0,0 +1,27 @@
+package homework_nr_2;
+
+public class HomeworkNr2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        long CurrentWorldPopulation = 7811080815L;
+        int CastProducedThisYear = 55235358;
+        char FirstLetterInAlphabet = 'A';
+        boolean JavaIsAmazing = true;
+        float Pi = 3.14159F;
+        System.out.println("Current world population = " + CurrentWorldPopulation);
+        System.out.println("Cast of this year produced population = " + CastProducedThisYear);
+        System.out.println("First letter of alphabet is - " + FirstLetterInAlphabet);
+        System.out.println("Java is amazing, that's - " + JavaIsAmazing);
+        System.out.println("Number Pi ~ " + Pi);
+        double Price = 123.345374D;
+        double Tax = 45.6873354D;
+        int Quantity = 345;
+        double Sum = 0D;
+        System.out.println("Price = " + Price);
+        System.out.println("Tax = " + Tax);
+        System.out.println("Quantity = " + Quantity);
+        System.out.println("Sum = " + Sum);
+        double total = Price + Tax * Quantity + Sum;
+        System.out.println("total = " + total);
+    }
+}
